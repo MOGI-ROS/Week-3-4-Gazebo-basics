42a0da74e3775f5e046fb019279ca70219567ab3
David Dudas <david.dudas@outlook.com>
2024-10-29 21:23:03 +0100

    add gazebo world and launch file to load it

---
+++ bme_gazebo_basics/launch/world.py
@@ -0,0 +1,42 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import  LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+
+    world_arg = DeclareLaunchArgument(
+        'world', default_value='world.sdf',
+        description='Name of the Gazebo world file to load'
+    )
+
+    pkg_bme_gazebo_basics = get_package_share_directory('bme_gazebo_basics')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    # Add your own gazebo library path here
+    gazebo_models_path = "/home/david/gazebo_models"
+    os.environ["GZ_SIM_RESOURCE_PATH"] = gazebo_models_path
+
+
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'),
+        ),
+        launch_arguments={'gz_args': [PathJoinSubstitution([
+            pkg_bme_gazebo_basics,
+            'worlds',
+            LaunchConfiguration('world')
+        ]),
+        TextSubstitution(text=' -r -v -v1 --render-engine ogre --render-engine-gui-api-backend opengl')],
+        'on_exit_shutdown': 'true'}.items()
+    )
+
+    launchDescriptionObject = LaunchDescription()
+
+    launchDescriptionObject.add_action(world_arg)
+    launchDescriptionObject.add_action(gazebo_launch)
+
+    return launchDescriptionObject
